ccd4c4a24b4518d7de577b288258030d8c227ad8
ct2112 <62403215+ct2112@users.noreply.github.com>
2020-03-19 15:06:56 -0600

    Put Documents into form wanted
    More error checks

---
+++ main.py
@@ -0,0 +1,18 @@
+﻿import sys
+from onetoone import *
+
+def main():
+    if len(sys.argv) != 3:
+        print("Wrong Number of Arguments")
+        return
+
+    st1 = sys.argv[1]
+    st2 = sys.argv[2]
+
+    val = isonetoone(st1, st2)
+    if val == 0:
+      print("false")
+    else:
+      print("true")
+
+main()
--- onetoone.py
+++ onetoone.py
@@ -1,36 +1,33 @@
 import sys
 
+def isInlist(inp, skip):
+
+    for i in skip:
+        if i == inp:
+            return True
+    return False 
+
 def isonetoone(st1, st2):
      #  1. check if you are the same as anyone later, 
      #  2. if they are, then the corresponding in str2 must be the same as well
    
+
     if len(st1) != len(st2):
+        print("Strings are not the same length")
         return False
+    
+    # contains values of positions already checked because they match a previous char
+    skip_check = []
 
     for i in range(len(st2)):
-        for j in range(i,len(st2)):
+        if isInlist(i, skip_check):
+            continue
 
+        for j in range(i+1,len(st2)):
             if st1[i] == st1[j]:
                 if st2[i] != st2[j]:
                     return False
+                else:
+                    skip_check.append(j)
+
     return True
-
-
-
-
-def main():
-    if len(sys.argv) != 3:
-        print("Wrong Number of Arguments")
-        return
-
-    st1 = sys.argv[1]
-    st2 = sys.argv[2]
-
-    val = isonetoone(st1, st2)
-
-    if val == 0:
-      print("false")
-    else:
-      print("true")
-
-main()
