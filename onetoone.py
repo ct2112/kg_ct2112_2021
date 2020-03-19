309d1441028afe99650406e33a483207ed0a9115
ct2112 <62403215+ct2112@users.noreply.github.com>
2020-03-19 15:25:53 -0600

    Changed the approach
    Used a more efficient approach of storing each value in a dictionary rather than checking every letter each iteration

--- onetoone.py
+++ onetoone.py
@@ -1,33 +1,23 @@
-import sys
-
 def isInlist(inp, skip):
-
     for i in skip:
         if i == inp:
             return True
     return False 
 
 def isonetoone(st1, st2):
-     #  1. check if you are the same as anyone later, 
-     #  2. if they are, then the corresponding in str2 must be the same as well
-   
-
+    # Cannot have a one to one map if lengths are different
     if len(st1) != len(st2):
         print("Strings are not the same length")
         return False
     
-    # contains values of positions already checked because they match a previous char
-    skip_check = []
+    already_checked = {}
 
-    for i in range(len(st2)):
-        if isInlist(i, skip_check):
-            continue
-
-        for j in range(i+1,len(st2)):
-            if st1[i] == st1[j]:
-                if st2[i] != st2[j]:
-                    return False
-                else:
-                    skip_check.append(j)
+    #  If the letter has been seen already in st1, check if the corresponding st2 values are the same
+    for i in range(len(st1)):
+        if st1[i] in already_checked: #if this is a duplicate
+            if already_checked[st1[i]] != st2[i]:
+                return False
+        else:  
+            already_checked[st1[i]] = st2[i]
 
     return True
